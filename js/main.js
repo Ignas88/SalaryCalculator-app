920e2a9a74562c582073f8e8541748bffcd66458
Ignas88 <ignas.c88@gmail.com>
2018-03-03 00:42:21 +0200

    fixed issue with 'NaN'

--- js/main.js
+++ js/main.js
@@ -6,23 +6,32 @@ window.Calculator = {
 
         //istraukiame checkbox kintamaji +2proc pridejimui
         var checkBox1 = document.getElementById("check1");
-
-        //nustatome visus kintamuosius mokesciu apskaiciavimui
-        var incomeTax = onPaper * 0.15;
-        var insurance = onPaper * 0.06;
         var pensionProc = checkBox1.checked ? 0.05 : 0.03;
-        var pension = onPaper * pensionProc;
-        var employerTax = onPaper * 0.3118;
-        var workplacePrice = onPaper + employerTax;
-        var toHands = onPaper - (incomeTax + insurance + pension);
 
-        //perkeliame kintamuosius i html faila
-        document.getElementById('incomeTax').innerHTML = incomeTax;
-        document.getElementById('insurance').innerHTML = insurance;
-        document.getElementById('pension').innerHTML = pension;
+        //nustatome visus kintamuosius mokesciu apskaiciavimui, jei neivestas atl ant popieriaus visos reiksmes = 0
+        if (onPaper > 0) {
+            var incomeTax = onPaper * 0.15;
+            var insurance = onPaper * 0.06;
+            var pension = onPaper * pensionProc;
+            var employerTax = onPaper * 0.3118;
+            var workplacePrice = onPaper + employerTax;
+            var toHands = onPaper - (incomeTax + insurance + pension);
+        } else {
+            var incomeTax = 0;
+            var insurance = 0;
+            var pension = 0;
+            var employerTax = 0;
+            var workplacePrice = 0;
+            var toHands = 0;
+        }
+
+        //perkeliame kintamuosius i html faila, po kablelio 2 skaiciai
+        document.getElementById('incomeTax').innerHTML = incomeTax.toFixed(2);
+        document.getElementById('insurance').innerHTML = insurance.toFixed(2);
+        document.getElementById('pension').innerHTML = pension.toFixed(2);
         document.getElementById('employerTax').innerHTML = employerTax.toFixed(2);
         document.getElementById('workplacePrice').innerHTML = workplacePrice;
-        document.getElementById('toHands').innerHTML = toHands;
+        document.getElementById('toHands').innerHTML = toHands.toFixed(2);
     },
 
     //mokesciu skaiciavimas nuo atlyginimo i rankas
@@ -36,24 +45,34 @@ window.Calculator = {
         //if funkcija procentu pakeitimui skaiciavimuose
         var pensionProc = checkBox1.checked ? 0.05 : 0.03;
 
-        //jei ivestas autoriniu teisiu atl, mokescius skaiciuojame su juo, kitaip be jo
-        if(onHandsCopyright > 0){
+        //jei neivestas kuris nors atl skaiciuojame be jo, jei nieko neivesta visos reiksmes = 0
+        if(onHandsCopyright > 0 && onHands > 0){
             var incomeTax = (onHands + onHandsCopyright) * 0.15;
             var insurance = (onHands + onHandsCopyright) * 0.06;
             var pension = (onHands + onHandsCopyright) * pensionProc;
             var onPaper = (onHands + onHandsCopyright) + incomeTax + insurance + pension;
-        }else {
+        }else if (onHands > 0) {
             var incomeTax = onHands * 0.15;
             var insurance = onHands * 0.06;
             var pension = onHands * pensionProc;
             var onPaper = onHands + incomeTax + insurance + pension;
+        } else if (onHandsCopyright > 0) {
+            var incomeTax = onHandsCopyright * 0.15;
+            var insurance = onHandsCopyright * 0.06;
+            var pension = onHandsCopyright * pensionProc;
+            var onPaper = onHandsCopyright + incomeTax + insurance + pension;
+        } else {
+            var incomeTax = 0;
+            var insurance = 0;
+            var pension = 0;
+            var onPaper = 0;
         }
 
-        //apskaiciuotus kintamuosius perkeliame i html faila
-        document.getElementById('incomeTax').innerHTML = incomeTax;
-        document.getElementById('insurance').innerHTML = insurance;
-        document.getElementById('pension').innerHTML = pension;
-        document.getElementById('onPaper').innerHTML = onPaper;
+        //apskaiciuotus kintamuosius perkeliame i html faila, po kablelio 2 skaiciai
+        document.getElementById('incomeTax').innerHTML = incomeTax.toFixed(2);
+        document.getElementById('insurance').innerHTML = insurance.toFixed(2);
+        document.getElementById('pension').innerHTML = pension.toFixed(2);
+        document.getElementById('onPaper').innerHTML = onPaper.toFixed(2);
     },
 
     copyright: function () {
@@ -62,13 +81,18 @@ window.Calculator = {
         var copyrightTax = parseFloat(document.getElementById('cTax').value);
         var startupTax = parseFloat(document.getElementById('startTax').value);
 
-        //nustatome kintamuosius ir ju apskaiciavimus
-        var authorToHands = incomeCopyright - ((incomeCopyright * copyrightTax)/100);
-        var startupPay = incomeCopyright + ((incomeCopyright * startupTax)/100);
+        //nustatome kintamuosius ir ju apskaiciavimus, jei nevestas atl ir procentai, visos reiksmes = 0
+        if(incomeCopyright > 0 && copyrightTax > 0 && startupTax > 0){
+            var authorToHands = incomeCopyright - ((incomeCopyright * copyrightTax)/100);
+            var startupPay = incomeCopyright + ((incomeCopyright * startupTax)/100);
+        } else {
+            var authorToHands = 0;
+            var startupPay = 0;
+        }
 
-        //apskaiciuotus kintamuosius perkeliame i html faila
-        document.getElementById('authorToHands').innerHTML = authorToHands;
-        document.getElementById('startupPay').innerHTML = startupPay;
+        //apskaiciuotus kintamuosius perkeliame i html faila, po kablelio 2 skaiciai
+        document.getElementById('authorToHands').innerHTML = authorToHands.toFixed(2);
+        document.getElementById('startupPay').innerHTML = startupPay.toFixed(2);
     }
 };
 //checkboxas keiciantis procentus pensiju draudime
