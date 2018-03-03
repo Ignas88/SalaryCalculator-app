8f9a0d65050a1cc37ccecbacb6a59b3d07722a3f
Ignas88 <ignas.c88@gmail.com>
2018-03-03 22:29:10 +0200

    code refactored part 3

--- js/main.js
+++ js/main.js
@@ -8,20 +8,15 @@ window.Calculator = {
         var pensionProc = $("#check1").prop("checked") ? 0.05 : 0.03;
 
         //nustatome visus kintamuosius mokesciu apskaiciavimui, jei neivestas atl ant popieriaus visos reiksmes = 0
+        var incomeTax = insurance = pension = employerTax = workplacePrice = toHands = 0;
+
         if (onPaper > 0) {
-            var incomeTax = onPaper * 0.15;
-            var insurance = onPaper * 0.06;
-            var pension = onPaper * pensionProc;
-            var employerTax = onPaper * 0.3118;
-            var workplacePrice = onPaper + employerTax;
-            var toHands = onPaper - (incomeTax + insurance + pension);
-        } else {
-            var incomeTax = 0;
-            var insurance = 0;
-            var pension = 0;
-            var employerTax = 0;
-            var workplacePrice = 0;
-            var toHands = 0;
+             incomeTax = onPaper * 0.15;
+             insurance = onPaper * 0.06;
+             pension = onPaper * pensionProc;
+             employerTax = onPaper * 0.3118;
+             workplacePrice = onPaper + employerTax;
+             toHands = onPaper - (incomeTax + insurance + pension);
         }
 
         //perkeliame kintamuosius i html faila, po kablelio 2 skaiciai
@@ -43,26 +38,23 @@ window.Calculator = {
         var pensionProc = $("#check1").prop("checked") ? 0.05 : 0.03;
 
         //jei neivestas kuris nors atl skaiciuojame be jo, jei nieko neivesta visos reiksmes = 0
+        var incomeTax = insurance = pension = onPaper = 0;
+
         if(onHandsCopyright > 0 && onHands > 0){
-            var incomeTax = (onHands + onHandsCopyright) * 0.15;
-            var insurance = (onHands + onHandsCopyright) * 0.06;
-            var pension = (onHands + onHandsCopyright) * pensionProc;
-            var onPaper = (onHands + onHandsCopyright) + incomeTax + insurance + pension;
+             incomeTax = (onHands + onHandsCopyright) * 0.15;
+             insurance = (onHands + onHandsCopyright) * 0.06;
+             pension = (onHands + onHandsCopyright) * pensionProc;
+             onPaper = (onHands + onHandsCopyright) + incomeTax + insurance + pension;
         }else if (onHands > 0) {
-            var incomeTax = onHands * 0.15;
-            var insurance = onHands * 0.06;
-            var pension = onHands * pensionProc;
-            var onPaper = onHands + incomeTax + insurance + pension;
+             incomeTax = onHands * 0.15;
+             insurance = onHands * 0.06;
+             pension = onHands * pensionProc;
+             onPaper = onHands + incomeTax + insurance + pension;
         } else if (onHandsCopyright > 0) {
-            var incomeTax = onHandsCopyright * 0.15;
-            var insurance = onHandsCopyright * 0.06;
-            var pension = onHandsCopyright * pensionProc;
-            var onPaper = onHandsCopyright + incomeTax + insurance + pension;
-        } else {
-            var incomeTax = 0;
-            var insurance = 0;
-            var pension = 0;
-            var onPaper = 0;
+             incomeTax = onHandsCopyright * 0.15;
+             insurance = onHandsCopyright * 0.06;
+             pension = onHandsCopyright * pensionProc;
+             onPaper = onHandsCopyright + incomeTax + insurance + pension;
         }
 
         //apskaiciuotus kintamuosius perkeliame i html faila, po kablelio 2 skaiciai
@@ -79,12 +71,11 @@ window.Calculator = {
         var startupTax = parseFloat($("#startTax").val());
 
         //nustatome kintamuosius ir ju apskaiciavimus, jei nevestas atl ir procentai, visos reiksmes = 0
+        var authorToHands = startupPay = 0;
+
         if(incomeCopyright > 0 && copyrightTax > 0 && startupTax > 0){
-            var authorToHands = incomeCopyright - ((incomeCopyright * copyrightTax)/100);
-            var startupPay = incomeCopyright + ((incomeCopyright * startupTax)/100);
-        } else {
-            var authorToHands = 0;
-            var startupPay = 0;
+             authorToHands = incomeCopyright - ((incomeCopyright * copyrightTax)/100);
+             startupPay = incomeCopyright + ((incomeCopyright * startupTax)/100);
         }
 
         //apskaiciuotus kintamuosius perkeliame i html faila, po kablelio 2 skaiciai
