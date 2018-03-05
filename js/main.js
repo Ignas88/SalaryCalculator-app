0f0120252084dc479582173463600c850bcaa860
Ignas88 <ignas.c88@gmail.com>
2018-03-05 11:54:53 +0200

    final code refactor

--- js/main.js
+++ js/main.js
@@ -1,25 +1,23 @@
 window.Calculator = {
     setProc: function () {
-             //istraukiame ivestus procentus, jei nieko neivestas kintamieji = 0
-             var incomeProc =  parseFloat($("#incomeProc").val()) || 0;
-             var insurProc = parseFloat($("#insurProc").val()) || 0;
-             var pensProc = parseFloat($("#pensProc").val()) || 0;
-             var emplProc = parseFloat($("#emplProc").val()) || 0;
-             var copProc = parseFloat($("#cTax").val()) || 0;
-             var startProc = parseFloat($("#startTax").val()) || 0;
+        //istraukiame ivestus procentus, jei nieko neivestas kintamieji = 0
+        var incomeProc = parseFloat($("#incomeProc").val()) || 0;
+        var insurProc = parseFloat($("#insurProc").val()) || 0;
+        var pensProc = parseFloat($("#pensProc").val()) || 0;
+        var emplProc = parseFloat($("#emplProc").val()) || 0;
+        var copProc = parseFloat($("#cTax").val()) || 0;
+        var startProc = parseFloat($("#startTax").val()) || 0;
 
-             //prekeliame kintamuosius i i kita html elementa
-             $("#incProc").html(incomeProc.toFixed(2));
-             $("#insProc").html(insurProc.toFixed(2));
-             $("#penProc").html(pensProc.toFixed(2));
-             $("#empProc").html(emplProc.toFixed(2));
-             $("#copProc").html(copProc.toFixed(2));
-             $("#startProc").html(startProc.toFixed(2));
+        //prekeliame kintamuosius i i kita html elementa
+        $("#incProc").html(incomeProc.toFixed(2));
+        $("#insProc").html(insurProc.toFixed(2));
+        $("#penProc").html(pensProc.toFixed(2));
+        $("#empProc").html(emplProc.toFixed(2));
+        $("#copProc").html(copProc.toFixed(2));
+        $("#startProc").html(startProc.toFixed(2));
 
-            //     uzdarome settings langa
-            if ($("#settingsWindow").css("display") == "block") {
-                $("#settingsWindow").hide();
-            }
+        //     uzdarome settings langa
+        $("#settingsWindow").toggle()
     },
     //mokesciu skaiciavimas nuo atlyginimo ant popieriaus
     onPaper: function () {
@@ -34,7 +32,7 @@ window.Calculator = {
         var onPaper = parseFloat($("#salary").val());
 
         //istraukiame checkbox kintamaji +2proc pridejimui su if funkcija procentu pakeitimui skaiciavimuose
-        if($("#check1").prop("checked")){
+        if ($("#check1").prop("checked")) {
             pensProc += 2;
         }
 
@@ -72,22 +70,19 @@ window.Calculator = {
         var onHandsCopyright = parseFloat($("#salary2").val());
 
         //istraukiame checkbox kintamaji +2proc pridejimui ir panaudojame su if funkcija
-        if($("#check1").prop("checked")){
+        if ($("#check1").prop("checked")) {
             pensProc += 2;
-        }else{
-            pensProc = 0;
         }
-
 
         //jei neivestas kuris nors atl skaiciuojame be jo, jei nieko neivesta visos reiksmes = 0
         var incomeTax = insurance = pension = onPaper = 0;
 
-        if(onHandsCopyright > 0 && onHands > 0){
+        if (onHandsCopyright > 0 && onHands > 0) {
              incomeTax = (onHands + onHandsCopyright) * (incomeProc/100);
              insurance = (onHands + onHandsCopyright) * (insurProc/100);
              pension = (onHands + onHandsCopyright) * (pensProc/100);
              onPaper = (onHands + onHandsCopyright) + incomeTax + insurance + pension;
-        }else if (onHands > 0) {
+        } else if (onHands > 0) {
              incomeTax = onHands * (incomeProc/100);
              insurance = onHands * (insurProc/100);
              pension = onHands * (pensProc/100);
@@ -115,7 +110,7 @@ window.Calculator = {
         //nustatome kintamuosius ir ju apskaiciavimus, jei nevestas atl ir procentai, visos reiksmes = 0
         var authorToHands = startupPay = 0;
 
-        if(incomeCopyright > 0 && copyrightTax > 0 && startupTax > 0){
+        if (incomeCopyright > 0 && copyrightTax > 0 && startupTax > 0) {
              authorToHands = incomeCopyright - ((incomeCopyright * copyrightTax)/100);
              startupPay = incomeCopyright + ((incomeCopyright * startupTax)/100);
         }
@@ -126,34 +121,25 @@ window.Calculator = {
     }
 };
 
-//checkboxas keiciantis procentus pensiju draudime
+//checkboxas keiciantis procentu texta pensiju draudime
 function boxCheck1() {
     var pensProc = parseFloat($("#pensProc").val()) || 0;
     //jei checkboxas pazymetas textas + 2, kitaip + 0
     if ($("#check1").prop("checked")) {
         pensProc += 2;
         $("#penProc").html(pensProc.toFixed(2));
-    } else{
-        pensProc = 0;
+    } else {
         $("#penProc").html(pensProc.toFixed(2));
     }
 }
 
 //checkboxas atidarantis ir uzdarantis autoriniu teisiu mokesciu apskaiciavimo langa
 function boxCheck2() {
-    if ($("#check2").prop("checked")){
-        $("#well").show();
-    } else {
-        $("#well").hide();
-    }
+    $('#well').toggle();
 }
 
 //paspaudus settings icona atsidaro/uzsidaro nustatymu langas
 function settingsClick() {
-    if ($("#settingsWindow").css("display") == "block") {
-        $("#settingsWindow").hide();
-    } else {
-        $("#settingsWindow").show();
-    }
+    $("#settingsWindow").toggle()
 }
 
